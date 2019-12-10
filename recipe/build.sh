65d6f355055e7dfab565524cb1311b8c7c976c61
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-12-10 07:16:09 -0700

    Use --enable-stl

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,14 +13,12 @@ cd build_unix
 ../dist/configure --prefix=$PREFIX \
                   --enable-shared \
                   --disable-static \
-                  --enable-cxx
+                  --enable-cxx \
+                  --enable-stl
 
 make -j$CPU_COUNT
 make check -j$CPU_COUNT
 make install -j$CPU_COUNT
-
-cp ../lang/cxx/stl/*.h ${PREFIX}/include
-cp ../build_windows/dbstl_common.h ${PREFIX}/include
 
 cd $PREFIX
 find . -type f -name "*.la" -exec rm -rf '{}' \; -print
